6ea697a363f071fe4cf5f376c3a57d73b7e012a7
Bani <borboleta@gmail.com>
2011-07-17 03:02:08 -0300

    efeitos sonoros

--- src/js/placar.js
+++ src/js/placar.js
@@ -1,7 +1,39 @@
 function down(pontos) {
     $("#" + pontos).text(parseInt($("#" + pontos).text()) - 1);
+    sound.playDown();
 }
 
 function up(pontos) {
     $("#" + pontos).text(parseInt($("#" + pontos).text()) + 1);
+    sound.playUp();
 }
+
+var sound = {
+    up: undefined,
+    down: undefined,
+    audioInit: function(){
+        try {
+            this.up = document.createElement('audio');
+            this.up.setAttribute('src', 'style/up.wav');
+            this.down = document.createElement('audio');
+            this.down.setAttribute('src', 'style/down.wav');
+        } 
+        catch (err) {}
+    },
+    playUp: function(){
+        try {
+            this.up.play();
+        } 
+        catch (err) {}
+    },
+    playDown: function(){
+        try {
+            this.down.play();
+        } 
+        catch (err) {}
+    },
+};
+
+$(document).ready(function(){
+    sound.audioInit();
+});
